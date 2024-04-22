23b42f2ee05446dd5e69d1065c9c21d8ffb88cd3
omrfrkcpr <omerrfarukcapur@gmail.com>
2024-04-22 22:02:39 +0200

    "Update AuthImage component and remove useStockCall hook"
    The AuthImage component's grid item style and max width have been updated. The useStockCall hook, which included code for making API calls and dispatching Redux

--- src/components/AuthImage.jsx
+++ src/components/AuthImage.jsx
@@ -2,14 +2,7 @@ import Container from "@mui/material/Container";
 import Grid from "@mui/material/Grid";
 const AuthImage = ({ image }) => {
   return (
-    <Grid
-      item
-      xs={6}
-      sm={7}
-      md={8}
-      lg={10}
-      style={{ margin: "auto", maxWidth: "300px" }}
-    >
+    <Grid item style={{ margin: "auto", maxWidth: "100px" }}>
       <Container>
         <img src={image} alt="img" style={{ width: "100%" }} />
       </Container>
